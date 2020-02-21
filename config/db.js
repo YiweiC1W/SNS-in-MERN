a5192ae5ecc4f79dcbdaec49ef832a26b34c0952
Yiwei Chen <kanyeishere@gmail.com>
2020-02-21 22:43:23 +1100

    add web token

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-const config = require('config')
+const mongoose = require('mongoose');
+const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
@@ -14,7 +14,6 @@ const connectDB = async () => {
         console.log(err.message);
         process.exit(1)
     }
-}
+};
 
 module.exports = connectDB;
-
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-const {check, validationResult} = require('express-validator/check');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User.model');
 
@@ -11,65 +13,63 @@ const User = require('../../models/User.model');
 // @desc   Register user
 // @access Public
 router.post(
-    '/',
-    [
-        check('name', 'Name is required')
-            .not()
-            .isEmpty(),
-        check('email', 'Please include a valid email').isEmail(),
-        check(
-            'password',
-            'Please enter a password with 6 or more characters'
-        ).isLength({min: 6})
+	'/',
+	[
+		check('name', 'Name is required').not().isEmpty(),
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()){
-            return res.status(400).json({errors:errors.array()})
-        }
+		const { name, email, password } = req.body;
 
-        const { name, email, password} = req.body;
+		try {
+			// See if user exists
+			let user = await User.findOne({ email });
+			if (user) {
+				res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
+			}
 
-        try{
-            let user = await  User.findOne({email});
+			// Get user gravatar
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm'
+			});
 
-            if (user){
-                res.status(400).json({errors:[{msg: 'User already exists'}]});
-            }
+			user = new User({
+				name,
+				email,
+				avatar,
+				password
+			});
 
-            // See if user exists
+			// Encrypt password
 
-            // Get user gravatar
-            const avatar = gravatar.url(email,{
-                s:'200',
-                r:'pg',
-                d:'mm'
-            });
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
 
-            user = new User({
-                name,
-                email,
-                avatar,
-                password
-            });
+			// Return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
 
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-            await user.save();
-
-            // Encrypt password
-
-            // Return jsonwebtoken
-            res.send('User registered');
-
-        }catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error')
-        }
-
-        res.send('User route');
-    }
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
 );
 
 module.exports = router;
